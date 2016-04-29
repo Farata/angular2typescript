fcfbe80fbaa47ac9ac15b2df71c021f6a51e5fef
Yakov Fain <yakovfain@gmail.com>
2016-04-29 05:20:48 -0400

    CH10: added missing typings.json to basic-webpack-starter

---
+++ chapter10/basic-webpack-starter/typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/zone.js/index.d.ts" />
---
+++ chapter10/basic-webpack-starter/typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/jasmine/index.d.ts" />
+/// <reference path="main/ambient/zone.js/index.d.ts" />
