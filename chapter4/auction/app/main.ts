d7f17a228f2b7be68359e09deb130031afe60eab
Yakov Fain <yakovfain@gmail.com>
2016-06-24 08:26:41 -0400

    CH4: updated to rc.3

--- chapter4/auction/app/main.ts
+++ chapter4/auction/app/main.ts
@@ -1,4 +1,4 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
+/*import {bootstrap} from '@angular/platform-browser-dynamic';
 import {provide} from '@angular/core';
 import { ROUTER_PROVIDERS} from '@angular/router';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
@@ -6,4 +6,24 @@ import ApplicationComponent from "./components/application/application";
 import {ProductService} from './services/product-service';
 bootstrap(ApplicationComponent, [
     ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), ProductService
+]);*/
+
+
+
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {provideRouter} from '@angular/router';
+
+import ApplicationComponent from './components/application/application';
+import HomeComponent from './components/home/home';
+import ProductDetailComponent from './components/product-detail/product-detail';
+import {ProductService} from './services/product-service';
+
+bootstrap(ApplicationComponent, [
+    provideRouter([
+        {path: '',                    component: HomeComponent},
+        {path: 'products/:productId', component: ProductDetailComponent}
+    ]),
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    ProductService
 ]);
