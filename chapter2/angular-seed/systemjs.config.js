d77404ba5abc7a557541198fc7594f2a2fc2d2a6
Yakov Fain <yakovfain@gmail.com>
2016-07-28 08:32:13 -0400

    CH2: Updated System config to minimize the number of network requests and size

--- chapter2/angular-seed/systemjs.config.js
+++ chapter2/angular-seed/systemjs.config.js
@@ -2,18 +2,20 @@ System.config({
     transpiler: 'typescript',
     typescriptOptions: {emitDecoratorMetadata: true},
     map: {
-        'app' : 'app',
-        'rxjs': 'node_modules/rxjs',
-        '@angular'                         : 'node_modules/@angular'
+      '@angular': 'node_modules/@angular'
+    },
+    paths: {
+      'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+    },
+    meta: {
+      '@angular/*': {'format': 'cjs'}
     },
     packages: {
-        'app'                              : {main: 'main.ts', defaultExtension: 'ts'},
-        'rxjs'                             : {main: 'index.js'},
-        '@angular/core'                    : {main: 'index.js'},
-        '@angular/common'                  : {main: 'index.js'},
-        '@angular/compiler'                : {main: 'index.js'},
-        '@angular/router'                  : {main: 'index.js'},
-        '@angular/platform-browser'        : {main: 'index.js'},
-        '@angular/platform-browser-dynamic': {main: 'index.js'}
+        'app'                              : {main: 'main', defaultExtension: 'ts'},
+       '@angular/core'                    : {main: 'core.umd.min.js'},
+       '@angular/common'                  : {main: 'common.umd.min.js'},
+       '@angular/compiler'                : {main: 'compiler.umd.min.js'},
+       '@angular/platform-browser'        : {main: 'platform-browser.umd.min.js'},
+       '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
     }
 });
