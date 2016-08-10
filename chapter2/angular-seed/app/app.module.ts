fc1f245ebcbe3d18aef03231d3df5f0ec3b33668
Yakov Fain <yakovfain@gmail.com>
2016-08-09 22:21:33 -0400

    ch2: separated bootstrap an ngModule in angular-seed

---
+++ chapter2/angular-seed/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from 'app/app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
--- chapter2/angular-seed/app/main.ts
+++ chapter2/angular-seed/app/main.ts
@@ -1,14 +1,6 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent }  from 'app/app.component';
-
-@NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
+import { AppModule }  from 'app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
