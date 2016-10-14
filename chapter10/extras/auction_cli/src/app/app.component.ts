acfe588d5de74c3824274f398c57ea216bb275b2
Yakov Fain <yakovfain@gmail.com>
2016-10-14 05:39:04 -0400

    CH10 extras: removed the viewencapsulation.none to make --aot work

--- chapter10/extras/auction_cli/src/app/app.component.ts
+++ chapter10/extras/auction_cli/src/app/app.component.ts
@@ -4,6 +4,6 @@ import {Component, ViewEncapsulation} from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation:ViewEncapsulation.None
+ // encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {}
