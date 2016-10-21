994ea725bf4621d2ca8fa55ba59348cd14aacbbf
Yakov Fain <yakovfain@gmail.com>
2016-10-21 07:52:43 -0400

    CH2: removed outdated aot sample

--- chapter2/angular-seed-aop/app/main.ts
+++
@@ -1,8 +0,0 @@
-// The browser platform without a compiler
-import { platformBrowser } from '@angular/platform-browser';
-
-// The app module factory produced by the static offline compiler
-import { AppModuleNgFactory } from './app.module.ngfactory';
-
-// Launch with the app module factory.
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
