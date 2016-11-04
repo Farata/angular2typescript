7d80ef4ec9e7317eb1fe33b283c4d32fb8affc67
Yakov Fain <yakovfain@gmail.com>
2016-11-04 08:48:20 -0400

    CH2 auction: added router to systemJS config?

--- chapter2/auction/systemjs.config.js
+++ chapter2/auction/systemjs.config.js
@@ -19,6 +19,7 @@ System.config({
         '@angular/core'                    : {main: 'core.umd.min.js'},
         '@angular/common'                  : {main: 'common.umd.min.js'},
         '@angular/compiler'                : {main: 'compiler.umd.min.js'},
+        '@angular/router'                  : {main: 'router.umd.min.js'},
         '@angular/platform-browser'        : {main: 'platform-browser.umd.min.js'},
         '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
     }
