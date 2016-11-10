5fb4519a172d919a01b32a797aa4d9f5a2ffc817
Yakov Fain <yakovfain@gmail.com>
2016-11-10 07:26:07 -0500

    Update vendor.ts

--- chapter10/angular2-webpack-starter/src/vendor.ts
+++ chapter10/angular2-webpack-starter/src/vendor.ts
@@ -1,6 +1,11 @@
 import 'zone.js/dist/zone';
 import 'reflect-metadata/Reflect.js';
 
+import '@angular/http';
+import '@angular/router';
+import '@angular/core';
+import '@angular/common';
+
 // RxJS - comment it our if not using or import specific functions
 //import 'rxjs';
 
