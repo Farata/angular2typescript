4b17c039873adb19fab49c0622d62cf49595a93b
Yakov Fain <yakovfain@gmail.com>
2016-12-03 15:05:17 -0500

    Update unsaved_changes.guard.ts

--- chapter3/router_samples/app/guards/unsaved_changes.guard.ts
+++ chapter3/router_samples/app/guards/unsaved_changes.guard.ts
@@ -1,12 +1,13 @@
 import {CanDeactivate, Router} from "@angular/router";
 import {Injectable} from "@angular/core";
+import {ProductDetailComponent} from "../components/product";
 
 @Injectable()
-export class UnsavedChangesGuard implements CanDeactivate{
+export class UnsavedChangesGuard implements CanDeactivate<ProductDetailComponent>{
 
     constructor(private _router:Router){}
 
-    canDeactivate(){
+    canDeactivate(component: ProductDetailComponent ){
         return window.confirm("You have unsaved changes. Still want to leave?");
 
     }
